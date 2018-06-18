041418cb1eb3f58fc448d74837099c1e8207914f
Holger Janßen-Kroll (hjk) <post@ho-jk.de>
2018-06-18 23:51:16 +0200

    minor: corrected some translation / description

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -2,8 +2,8 @@
 
 $GLOBALS['TL_LANG']['CTE']['hoja_newsletter'] = "(HoJa) Newsletter";
 $GLOBALS['TL_LANG']['FMD']['hoja_newsletter'] = "(HoJa) Newsletter";
-$GLOBALS['TL_LANG']['FMD']['hoja_nl_unsubscribe_double'] = "Austragen / Kündigen (Double-Opt-Out)";
-$GLOBALS['TL_LANG']['FMD']['hoja_nl_subscribe_with_name'] = "Eintragen / Abonnieren (mit Namen und Anrede)";
+$GLOBALS['TL_LANG']['FMD']['hoja_nl_unsubscribe_double'] = array ( "Newsletter Austragen / Kündigen", "Austragen / Kündigen (Double-Opt-Out)" );
+$GLOBALS['TL_LANG']['FMD']['hoja_nl_subscribe_with_name'] = array ( "Newsletter Eintragen / Abonnieren", "Eintragen / Abonnieren (mit Namen und Anrede)" );
 
 $GLOBALS['TL_LANG']['CTE']['hoja_nl_header'] = array('Newsletter-Header', 'Newsletter-Header mit Inhaltsverzeichnis');
 $GLOBALS['TL_LANG']['FMD']['hoja_nl_reader'] = array('Newsletterleser (HoJa)', 'Newsletter-Reader, der Content-Elemente mit anzeigt');
